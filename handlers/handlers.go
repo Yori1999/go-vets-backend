66db84eaa72e138028b7cea44cf5e5b8cbfebacf
Yori1999 <mariaf987@gmail.com>
2023-05-09 02:01:20 +0200

    v1

---
+++ handlers/handlers.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"project/go-vets-backend/middlewares"
+	"project/go-vets-backend/routers"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
+
+func Handlers() {
+	router := mux.NewRouter()
+	router.HandleFunc("/signUp", middlewares.CheckDB(routers.SignUp)).Methods("POST")
+	router.HandleFunc("/login", middlewares.CheckDB(routers.Login)).Methods("POST")
+	router.HandleFunc("/getUser", middlewares.CheckDB(middlewares.CheckJWT(routers.GetUser))).Methods("GET")
+	router.HandleFunc("/updateUser", middlewares.CheckDB(middlewares.CheckJWT(routers.UpdateUser))).Methods("PUT")
+
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "8000"
+	}
+
+	handler := cors.AllowAll().Handler(router)
+	log.Fatal(http.ListenAndServe(":"+PORT, handler))
+}
